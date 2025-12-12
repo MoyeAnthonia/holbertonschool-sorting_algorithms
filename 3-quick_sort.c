48281ae49d4cb52f2710037e5a05f7cd2b7bb93c
MoyeAnthonia <moye.anthonia@gmail.com>
2025-12-12 13:19:54 +0000

    create files

---
+++ 3-quick_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/**
+ * quick_sort - sorts an array of integers in asc order
+ * @array: array to sort
+ * @size: size of the array
+ */
+void quick_sort(int *array, size_t size)
+{
+
+}
